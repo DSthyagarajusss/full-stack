829060b0e05b87a514439e33e1ded7362c0d3d65
DSthyagarajusss <dsthyaraju@gmail.com>
2024-07-03 17:14:48 +0530

    javascript

---
+++ javascript/data1.js
@@ -0,0 +1,15 @@
+
+var price="499"
+var Rating="4.9"
+var brand="free"
+var prod_Name="mens chack shirt"
+var avail="true"
+var discount;
+
+
+console.log(typeof brand)
+console.log(typeof price)
+console.log(typeof Rating)
+console.log(typeof prod_Name)
+console.log(typeof avail)
+console.log(typeof discount)

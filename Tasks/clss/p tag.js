905d97df4090be7c0854a5a15173c8c0fce979d1
DSthyagarajusss <dsthyaraju@gmail.com>
2024-07-24 17:45:55 +0530

    jj

---
+++ Tasks/clss/p tag.js
@@ -0,0 +1,5 @@
+//document.getElementById('h1_tag').innerHTML="good morning"
+
+document.getElementsByTagName('h1')[0].innerHTML='Good Morning'
+
+//document.querySelector('.abc').innerHTML='good mornimg'
---
+++ javascript/oops/objects/account.js
@@ -0,0 +1,22 @@
+
+class account{
+    account_id=101
+    account_user_Name="Thyaga"
+    account_bal=200000
+
+    open_Account(){
+    console.log("Account open successfuly")
+    }
+    deposit_amount(){
+        console.log('amount diposit successfuly')
+    }
+    withdrawl(){
+        console.log('no money')
+    }
+}
+let a1 = new account()
+ a1.open_Account()
+ a1.deposit_amount()
+ a1.withdrawl()
+ console.log(a1)
+

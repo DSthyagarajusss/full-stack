4b8e5d00fd777bfbdba2d740c9449aa35c6706a0
DSthyagarajusss <dsthyaraju@gmail.com>
2024-08-06 18:18:14 +0530

    'h'

--- React js/my-app/src/App.js
+++ React js/my-app/src/App.js
@@ -1,47 +1,7 @@
-/*
-
-function 
-
-import Footer from "./class/Footer copy"
-let App =()=> {
-  return <div>
-    <h1>componets</h1>
-    <Header/>
-    <Slider/>
-    <Footer/>
-    <Mass/>
-  </div>
-}
-
-export default App
-
-
-//class
-import React from "react";
-import Mask from './fuction com/mass/Mask'
-class App extends React.Component{
-  render(){
-    return <>
-    <h1>
-      hii
-    </h1>
-    <p>class componets</p>
-    <Mask/>
-    </>
-  }
-}
-export default App*/
-import React from "react";
-import Footer from "./class/Header";
-import Header from "./class/Slider";
-import Slider from "./class/Footer";
-class App extends React.Component{
-  render(){
-    return <>
-    <Header/><hr/>
-    <Slider/><hr />
-    <Footer/><hr />
-    </>
-  }
+import Nav from "./bootstrap/Nav"
+let App =()=>{
+  return <>
+  <Nav/>
+  </>
 }
 export default App
